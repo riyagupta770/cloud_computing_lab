6f1c654106fae9ea16efe047db776949a1f0a06b
Riya Gupta <65082616+riyagupta770@users.noreply.github.com>
2023-10-29 22:59:53 +0530

    bst.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -1,32 +1,180 @@
-// C++ code to demonstrate star pattern
-
 #include <iostream>
-using namespace std;
 
-// Function to demonstrate printing pattern
-void pypart(int n)
-{
-	// Outer loop to handle number of rows
-	// n in this case
-	for (int i = 0; i < n; i++) {
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
 
-		// Inner loop to handle number of columns
-		// values changing acc. to outer loop
-		for (int j = 0; j <= i; j++) {
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
 
-			// Printing stars
-			cout << "* ";
-		}
+class BinarySearchTree {
+private:
+    Node* root;
 
-		// Ending line after each row
-		cout << endl;
-	}
-}
+    Node* insert(Node* root, int data) {
+        if (root == nullptr) {
+            return new Node(data);
+        }
 
-// Driver Function
-int main()
-{
-	int n = 5;
-	pypart(n);
-	return 0;
+        if (data < root->data) {
+            root->left = insert(root->left, data);
+        }
+        else if (data > root->data) {
+            root->right = insert(root->right, data);
+        }
+
+        return root;
+    }
+
+    Node* findMin(Node* node) {
+        while (node->left != nullptr) {
+            node = node->left;
+        }
+        return node;
+    }
+
+    Node* remove(Node* root, int data) {
+        if (root == nullptr) {
+            return root;
+        }
+
+        if (data < root->data) {
+            root->left = remove(root->left, data);
+        }
+        else if (data > root->data) {
+            root->right = remove(root->right, data);
+        }
+        else {
+            if (root->left == nullptr) {
+                Node* temp = root->right;
+                delete root;
+                return temp;
+            }
+            else if (root->right == nullptr) {
+                Node* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            Node* temp = findMin(root->right);
+            root->data = temp->data;
+            root->right = remove(root->right, temp->data);
+        }
+
+        return root;
+    }
+
+    bool search(Node* root, int data) {
+        if (root == nullptr) {
+            return false;
+        }
+
+        if (data == root->data) {
+            return true;
+        }
+        else if (data < root->data) {
+            return search(root->left, data);
+        }
+        else {
+            return search(root->right, data);
+        }
+    }
+
+    int getHeight(Node* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(node->left);
+        int rightHeight = getHeight(node->right);
+
+        return 1 + std::max(leftHeight, rightHeight);
+    }
+
+    bool isBalanced(Node* node) {
+        if (node == nullptr) {
+            return true;
+        }
+
+        int leftHeight = getHeight(node->left);
+        int rightHeight = getHeight(node->right);
+
+        if (abs(leftHeight - rightHeight) <= 1 &&
+            isBalanced(node->left) &&
+            isBalanced(node->right)) {
+            return true;
+        }
+
+        return false;
+    }
+
+public:
+    BinarySearchTree() : root(nullptr) {}
+
+    void insert(int data) {
+        root = insert(root, data);
+    }
+
+    void remove(int data) {
+        root = remove(root, data);
+    }
+
+    bool search(int data) {
+        return search(root, data);
+    }
+
+    void inorderTraversal(Node* node) {
+        if (node == nullptr) {
+            return;
+        }
+        inorderTraversal(node->left);
+        std::cout << node->data << " ";
+        inorderTraversal(node->right);
+    }
+
+    void inorder() {
+        inorderTraversal(root);
+        std::cout << std::endl;
+    }
+
+    int getHeight() {
+        return getHeight(root);
+    }
+
+    bool isBalanced() {
+        return isBalanced(root);
+    }
+};
+
+int main() {
+    BinarySearchTree bst;
+
+    bst.insert(50);
+    bst.insert(30);
+    bst.insert(20);
+    bst.insert(40);
+    bst.insert(70);
+    bst.insert(60);
+    bst.insert(80);
+
+    std::cout << "Inorder Traversal: ";
+    bst.inorder();
+    
+    std::cout << "Height of the tree: " << bst.getHeight() << std::endl;
+
+    if (bst.isBalanced()) {
+        std::cout << "The tree is balanced." << std::endl;
+    } else {
+        std::cout << "The tree is not balanced." << std::endl;
+    }
+
+    bst.remove(30);
+    bst.remove(80);
+
+    std::cout << "Inorder Traversal after removing nodes: ";
+    bst.inorder();
+
+    return 0;
 }
